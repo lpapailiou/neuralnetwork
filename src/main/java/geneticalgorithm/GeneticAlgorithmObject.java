db78088f3499dc61841f0b30b6f11fb4e63595ca
lpapailiou <lena@kaiki.ch>
2021-01-24 17:38:33 +0100

    version 1.9

---
+++ src/main/java/geneticalgorithm/GeneticAlgorithmObject.java
@@ -0,0 +1,38 @@
+package geneticalgorithm;
+
+import neuralnet.NeuralNetwork;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public abstract class GeneticAlgorithmObject<T> implements Comparable<GeneticAlgorithmObject<T>> {
+
+    private NeuralNetwork neuralNetwork;
+
+    public GeneticAlgorithmObject (NeuralNetwork neuralNetwork) {
+        this.neuralNetwork = neuralNetwork;
+    }
+
+    protected List<Double> predict(@NotNull double[] inputValues) {
+        return neuralNetwork.predict(inputValues);
+    }
+
+    public NeuralNetwork getNeuralNetwork() {
+        return neuralNetwork;
+    }
+
+    public abstract long getFitness();
+
+    @Override
+    public int compareTo(@NotNull GeneticAlgorithmObject<T> other) {
+        long fitness = this.getFitness();
+        long otherFitness = other.getFitness();
+        if (fitness > otherFitness) {
+            return 1;
+        } else if (fitness < otherFitness) {
+            return -1;
+        }
+        return 0;
+    }
+
+}
