7da70ee01ebe4fdd51c4d4ba5b8d7557be655a31
lpapailiou <lena@kaiki.ch>
2021-01-28 02:15:41 +0100

    adaptions

--- src/test/java/neuralnet/LearningRateDecreaseTest.java
+++ src/test/java/neuralnet/LearningRateDecreaseTest.java
@@ -7,10 +7,10 @@ public class LearningRateDecreaseTest {
 
     @Test
     public void learningRateDescentTest() {
-        LearningRateDescent lrd = LearningRateDescent.NONE;
+        LearningRateDescent lrd = LearningRateDescent.SGD;
         double lr = 0.8;
-        for (int i = 0; i < 30; i++) {
-            lr = lrd.decrease(lr, 0.001, i);
+        for (int i = 0; i < 100; i++) {
+            lr = lrd.decrease(lr, 0.0002, i);
             System.out.println(lr);
         }
     }
